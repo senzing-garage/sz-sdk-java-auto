592b1f38ee79d0014e5949d9f9f8259c214dd66e
Barry M. Caceres <barrycaceres@icloud.com>
2025-09-22 16:54:54 -0700

    Temporary change to attempt to reproduce access violation

---
+++ src/test/java/com/senzing/sdk/core/perpetual/WindowsAccessViolation.java
@@ -0,0 +1,150 @@
+package com.senzing.sdk.core.perpetual;
+
+import com.senzing.sdk.*;
+import com.senzing.sdk.core.*;
+
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_ALL_FEATURES;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_ENTITY_NAME;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_INTERNAL_FEATURES;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_DATA;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_FEATURES;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_JSON_DATA;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_MATCHING_INFO;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_TYPES;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_UNMAPPED_DATA;
+import static com.senzing.sdk.SzFlag.SZ_INCLUDE_FEATURE_SCORES;
+import static com.senzing.sdk.SzFlag.SZ_INCLUDE_MATCH_KEY_DETAILS;
+import static com.senzing.sdk.SzFlag.SZ_ENTITY_INCLUDE_RECORD_SUMMARY;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.EnumSet;
+import java.util.concurrent.*;
+
+public class WindowsAccessViolation {
+    public static void main(String[] args) {
+        SzEnvironment env = null;
+        ExecutorService executor = null;
+
+        if (args.length < 1) {
+            System.err.println("Must specify the settings path");
+            System.exit(1);
+        }
+        File senzingDir = new File(args[0]);
+        if (!senzingDir.exists() || !senzingDir.isDirectory()) {
+            System.err.println("Senzing path is invalid: " + senzingDir);
+            System.exit(1);
+        }
+        File erDir = new File(senzingDir, "er");
+        if (!erDir.exists() || !erDir.isDirectory()) {
+            System.err.println("Senzing path is invalid (er directory not found): " + senzingDir);
+            System.exit(1);
+        }
+        File configPath = new File(erDir, "etc");
+        File supportDir = new File(senzingDir, "data");
+        File resourceDir = new File(erDir, "resources");
+
+        File templateDir = new File(resourceDir, "templates");
+        File dbTemplateFile = new File(templateDir, "G2C.db");
+
+        try {
+
+            File tempDatabaseFile = File.createTempFile("G2C-", ".db");
+            Files.copy(dbTemplateFile.toPath(), 
+                    tempDatabaseFile.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            String settings = """
+                    {
+                        "PIPELINE": {
+                            "SUPPORTPATH": "%s",
+                            "RESOURCEPATH": "%s",
+                            "CONFIGPATH": "%s"
+                        },
+                        "SQL": {
+                            "CONNECTION": "sqlite3://na:na@/%s"
+                        }
+                    }
+                    """.formatted(supportDir.getCanonicalPath().replace('\\', '/'),
+                    resourceDir.getCanonicalPath().replace('\\', '/'),
+                    configPath.getCanonicalPath().replace('\\', '/'),
+                    tempDatabaseFile.getCanonicalPath().replace('\\', '/'));
+
+    	    System.out.println();
+            System.out.println(settings);
+            System.out.println();
+
+            env = SzCoreEnvironment.newBuilder().settings(settings).verboseLogging(false).build();
+
+            SzConfigManager configMgr = env.getConfigManager();
+            SzConfig config = configMgr.createConfig();
+            configMgr.setDefaultConfig(config.export());
+
+            SzEngine engine = env.getEngine();
+            String testRecord = """
+                    {
+                        "DATA_SOURCE": "TEST",
+                        "RECORD_ID": "ABC123",
+                        "NAME_FULL": "Joe Schmoe",
+                        "EMAIL_ADDRESS": "joeschmoe@nowhere.com",
+                        "PHONE_NUMBER": "702-555-1212"
+                    }
+                    """;
+            SzRecordKey recordKey = SzRecordKey.of("TEST", "ABC123");
+            engine.addRecord(recordKey, testRecord);
+            // process the redo records
+            for (String redo = engine.getRedoRecord();
+                    redo != null;
+                    redo = engine.getRedoRecord()) 
+            {
+                engine.processRedoRecord(redo);
+            }
+            String entityJson = engine.getEntity(
+                recordKey, EnumSet.of(SZ_ENTITY_INCLUDE_ALL_FEATURES,
+                                      SZ_ENTITY_INCLUDE_ENTITY_NAME,
+                                      SZ_ENTITY_INCLUDE_RECORD_SUMMARY,
+                                      SZ_ENTITY_INCLUDE_RECORD_TYPES,
+                                      SZ_ENTITY_INCLUDE_RECORD_DATA,
+                                      SZ_ENTITY_INCLUDE_RECORD_JSON_DATA,
+                                      SZ_ENTITY_INCLUDE_RECORD_MATCHING_INFO,
+                                      SZ_ENTITY_INCLUDE_RECORD_UNMAPPED_DATA,
+                                      SZ_ENTITY_INCLUDE_RECORD_FEATURES,
+                                      SZ_ENTITY_INCLUDE_INTERNAL_FEATURES,
+                                      SZ_INCLUDE_MATCH_KEY_DETAILS,
+                                      SZ_INCLUDE_FEATURE_SCORES));
+            env.destroy();
+            env = null;
+
+            env = SzCoreEnvironment.newBuilder().settings(settings).verboseLogging(false).build();
+            executor = Executors.newFixedThreadPool(4);
+            
+            SzDiagnostic diagnostic = env.getDiagnostic();
+
+            Future<String> future = executor.submit(() -> {
+                return diagnostic.getRepositoryInfo();
+            });
+
+            System.out.println(future.get());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            if (env != null) {
+                env.destroy();
+            }
+            if (executor != null) {
+                executor.shutdown();
+                while (!executor.isTerminated()) {
+                    try {
+                        executor.awaitTermination(5, TimeUnit.SECONDS);
+                    } catch (InterruptedException ignore) {
+                        // do nothing
+                    }
+                }
+            }
+        }
+    }
+}
